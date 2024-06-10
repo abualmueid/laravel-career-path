ca9a7db4924d025b1cc8529cf4097cdab79f565b
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-06-10 18:10:07 +0600

    modified file

--- Problem-1/FindMinVal.php
+++ Problem-1/FindMinVal.php
@@ -2,9 +2,10 @@
 
 $numbers = [10, -12, 34, 34, 12, -3, 123];
 
-function findMinVal($numbers): void {
+function findMinVal($numbers): int {
     $min = $numbers[0];
-    for($i = 1; $i < $numbers; $i++) {
+    $number = 0;
+    for($i = 1; $i < count($numbers); $i++) {
         // check absolute value
         if($numbers[$i] < 0) {
             $number = abs($numbers[$i]);
