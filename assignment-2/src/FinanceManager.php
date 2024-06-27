7343b57bfa20f06e195e32022ed55255c3c94151
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-06-27 17:02:56 +0600

    added file under src folder

---
+++ assignment-2/src/FinanceManager.php
@@ -0,0 +1,29 @@
+<?php 
+
+namespace CLIApp;
+
+class FinanceManager {
+    public function addIncome($amount, $category) {
+        echo "categories";
+    }
+
+    public function addExpense($amount, $category) {
+        echo "categories";
+    }
+
+    public function viewIncome() {
+        echo "categories";
+    }
+
+    public function viewExpense() {
+        echo "categories";
+    }
+
+    public function viewSavings() {
+        echo "categories";
+    }
+
+    public function viewCategories() {
+        echo "categories";
+    }
+}
---
+++ assignment-2/src/PersonalFinance.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace CLIApp;
+
+class PersonalFinance {
+    private FinanceManager $financeManager;
+
+    private const ADD_INCOME = 1;
+    private const ADD_EXPENSE = 2;
+    private const VIEW_INCOME = 3;
+    private const VIEW_EXPENSE = 4;
+    private const VIEW_SAVINGS = 5;
+    private const VIEW_CATEGORIES = 6;
+    private const EXIT_APP = 7;
+
+    private array $options = [
+        self::ADD_INCOME => "Add income", 
+        self::ADD_EXPENSE => "Add expense",
+        self::VIEW_INCOME => "View Income",
+        self::VIEW_EXPENSE => "View Expense",
+        self::VIEW_SAVINGS => "View Savings",
+        self::VIEW_CATEGORIES => "View Categories",
+        self::EXIT_APP => 'Exit App'
+    ];
+
+    public function __construct() {
+        $this->financeManager = new FinanceManager();
+    }
+
+    public function run(): void {
+        // echo "hello";
+        while (true) {
+            foreach ($this->options as $option => $label) {
+                printf("%d. %s\n", $option, $label);
+            }
+
+            $choice = intval(readline("Enter your option: "));
+
+            switch ($choice) {
+                case self::ADD_INCOME:
+                    $amount = floatval(trim(readline("Enter income amount: ")));
+                    $category = trim(readline("Enter income category: "));
+                    $this->financeManager->addIncome($amount, $category);
+                    break;
+                case self::ADD_EXPENSE:
+                    $amount = floatval(trim(readline("Enter expense amount: ")));
+                    $category = trim(readline("Enter expense category: "));
+                    $this->financeManager->addExpense($amount, $category);
+                    break;
+                case self::VIEW_INCOME:
+                    $this->financeManager->viewIncome();
+                    break;
+                case self::VIEW_EXPENSE:
+                    $this->financeManager->viewExpense();
+                    break;
+                case self::VIEW_SAVINGS:
+                    $this->financeManager->viewSavings();
+                    break;
+                case self::VIEW_CATEGORIES:
+                    $this->financeManager->viewCategories();
+                    break;
+                case self::EXIT_APP:
+                    return;
+                default:
+                    echo "Invalid option.\n";
+
+            }
+        }
+    }
+}
