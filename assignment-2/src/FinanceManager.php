a810780a9abc17c8429612d925f4493f8db3d544
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-06-29 12:31:20 +0600

    modified file

--- assignment-2/src/FinanceManager.php
+++ assignment-2/src/FinanceManager.php
@@ -16,24 +16,35 @@ class FinanceManager {
         echo "Income added successfully!\n";
     }
 
+    public function addExpense(float $amount, string $category) {
+        $this->expenses[] = [
+            "amount" => $amount,
+            "category" => $category
+        ];
+        $this->saveData();
+        echo "Expense added successfully!\n";
+    }
+
     private function saveData(): void {
         $data = [
             "incomes" => $this->incomes,
             "expenses" => $this->expenses
         ];
-        file_put_contents($this->dataFile, json_encode($data));
+        file_put_contents("src/" . $this->dataFile, json_encode($data));
     }
 
-    public function addExpense(float $amount, string $category) {
-        // echo "Expense added successfully!\n";
+    public function viewIncome(): void {
+        echo "Incomes:\n";
+        foreach($this->incomes as $income) {
+            echo "Amount: {$income['amount']}, Category: {$income['category']}\n";
+        }
     }
 
-    public function viewIncome() {
-        echo "categories";
-    }
-
-    public function viewExpense() {
-        echo "categories";
+    public function viewExpense():void {
+        echo "Expenses:\n";
+        foreach($this->expenses as $expense) {
+            echo "Amount: {$expense['amount']}, Category: {$expense['category']}\n";
+        }
     }
 
     public function viewSavings() {
