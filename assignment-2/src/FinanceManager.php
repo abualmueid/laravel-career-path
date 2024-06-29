ee7d2e7a08a44b73454b14dc182759dd1bea5432
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-06-29 18:10:21 +0600

    modified file

--- assignment-2/src/FinanceManager.php
+++ assignment-2/src/FinanceManager.php
@@ -16,13 +16,45 @@ class FinanceManager {
         echo "Income added successfully!\n";
     }
 
-    public function addExpense(float $amount, string $category) {
+    public function addExpense(float $amount, string $category): void {
         $this->expenses[] = [
             "amount" => $amount,
             "category" => $category
         ];
         $this->saveData();
         echo "Expense added successfully!\n";
+    }
+
+    public function viewIncome(): void {
+        echo "Incomes:\n";
+        $this->loadData();
+        foreach($this->incomes as $income) {
+            echo "Amount: {$income["amount"]}, Category: {$income["category"]}\n";
+        }
+    }
+
+    public function viewExpense(): void {
+        echo "Expenses:\n";
+        $this->loadData();
+        foreach($this->expenses as $expense) {
+            echo "Amount: {$expense['amount']}, Category: {$expense['category']}\n";
+        }
+    }
+
+    public function viewSavings(): void {
+        $totalIncome = array_sum(array_column($this->incomes, 'amount'));
+        $totalExpense = array_sum(array_column($this->expenses, 'amount'));
+        $savings = $totalIncome - $totalExpense;
+        echo "Savings: $savings\n";
+    }
+
+    public function viewCategories(): void {
+        $incomeCategories = array_unique(array_column($this->incomes, 'category'));
+        $expenseCategories = array_unique(array_column($this->expenses, 'category'));
+        $incomeCategories = implode(', ', $incomeCategories);
+        $expenseCategories = implode(', ', $expenseCategories);
+        echo "Income categories: $incomeCategories\n";
+        echo "Expense categories: $expenseCategories\n";
     }
 
     private function saveData(): void {
@@ -33,25 +65,11 @@ class FinanceManager {
         file_put_contents("src/" . $this->dataFile, json_encode($data));
     }
 
-    public function viewIncome(): void {
-        echo "Incomes:\n";
-        foreach($this->incomes as $income) {
-            echo "Amount: {$income['amount']}, Category: {$income['category']}\n";
+    private function loadData(): void {
+        if (file_exists($this->dataFile)) {
+            $data = json_decode(file_get_contents($this->dataFile), true);
+            $this->incomes = $data['incomes'];
+            $this->expenses = $data['expenses'];
         }
-    }
-
-    public function viewExpense():void {
-        echo "Expenses:\n";
-        foreach($this->expenses as $expense) {
-            echo "Amount: {$expense['amount']}, Category: {$expense['category']}\n";
-        }
-    }
-
-    public function viewSavings() {
-        echo "categories";
-    }
-
-    public function viewCategories() {
-        echo "categories";
     }
 }
