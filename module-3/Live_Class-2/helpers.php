2d0f6cf59abc8cac4e4d1dcc6191816a98ea27f6
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-07-03 20:33:27 +0600

    added file

---
+++ module-3/Live_Class-2/helpers.php
@@ -0,0 +1,12 @@
+<?php 
+
+function sanitize(string $data): string {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
+function dd(mixed $data): void {
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+    die();
+}
