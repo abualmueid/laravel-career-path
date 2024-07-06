125496db576a4643b2746105b7d7d4f6070efe8d
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-07-06 16:00:19 +0600

    modified file

--- module-3/Live_Class-2/db.php
+++ module-3/Live_Class-2/db.php
@@ -1,16 +1,17 @@
 <?php 
 
-$host = "localhost";
+$servername = "localhost";
 $dbname = "veronica";
 $username = "root";
 $password = "";
 
 try {
-    $dsn = "mysql:host=$host;dbname=$dbname";
-    $pdo = new PDO($dsn);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-    exit();
+  $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+  exit();
 }
