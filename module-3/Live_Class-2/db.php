20bcc871485c51bf5f107cdb081e5541c3289263
abualmueid <61595430+abualmueid@users.noreply.github.com>
2024-07-06 23:16:15 +0600

    modified file

--- module-3/Live_Class-2/db.php
+++ module-3/Live_Class-2/db.php
@@ -10,7 +10,7 @@ try {
   // set the PDO error mode to exception
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  echo "Connected successfully";
+  // echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
   exit();
